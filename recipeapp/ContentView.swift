c4ee250a725cc62420ccb4f0e49adc0d79f3c703
Gonzalo Caira <gcairaaguilar@gmail.com>
2023-05-07 14:25:57 -0500

    Initial Commit

---
+++ recipeapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  recipeapp
+//
+//  Created by heygonza on 7/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipeapp/recipeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeappApp.swift
+//  recipeapp
+//
+//  Created by heygonza on 7/05/23.
+//
+
+import SwiftUI
+
+@main
+struct recipeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
